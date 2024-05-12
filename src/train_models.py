d57462aea8c26bbd5aa9f8f5615299cbc73f9477
KCSchmutz <577511@student.belgiumcampus.ac.za>
2024-05-12 02:00:41 +0200

    changes on py files
    mainly the website design and source files

---
+++ src/train_models.py
@@ -0,0 +1,52 @@
+"begin"
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import silhouette_score
+import pandas as pd
+
+df = pd.DataFrame()
+"""function to create the model"""
+def getClusters(df):
+
+    #model creation
+    kmeans = KMeans(n_clusters= 3,max_iter= 50)
+    kmeans.fit(df)
+    lbs = kmeans.labels_
+
+    "elbow-method"
+    #appendin inertia
+    #wss
+    wss =[]
+    range_n_clusters = [2, 3, 4, 5, 6, 7, 8]
+    for num_clusters in range_n_clusters:
+        kmeans = KMeans(n_clusters= num_clusters, max_iter= 50)
+        kmeans.fit(df)
+        wss.append(kmeans.inertia_)
+    #silhouette score
+    n_cluster=0
+    silhouette_no=0
+    range_n_clusters = [2, 3, 4, 5, 6, 7, 8]
+    for num_clusters in range_n_clusters:
+        kmeans = KMeans(n_clusters= num_clusters, max_iter= 50)
+        kmeans.fit(df)
+        cluster_labels = kmeans.labels_
+        silhouette_avg = silhouette_score(df, cluster_labels)
+        print('For n_clusters{0}, the silhouette score is {1}'.format(num_clusters, silhouette_avg))
+        if silhouette_avg>silhouette_no:
+            silhouette_no=silhouette_avg
+            n_cluster=num_clusters
+    return n_cluster
+
+"returns the final data-model"
+def create_finalmodel(df):
+    # final_model labels
+    final_model = make_pipeline(
+        StandardScaler(),
+        KMeans(n_clusters=3, random_state=42)
+    )
+    # Predict class labels
+    labels = final_model.fit_predict(df)
+    return labels
+"end"
---
+++ src/web_app.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import dash
+from dash import dcc, html, Input, Output
+import base64
+import io
+import plotly.graph_objs as go
+import seaborn as sns
+from prepare_data import prepare_dataset
+from preprocess_data import process_data
+from train_models import create_finalmodel
+import dash_bootstrap_components as dbc
+
+"palettes"
+sns.color_palette("rocket", as_cmap=True)
+"outliers, no_outliers"
+df = pd.DataFrame()
+def prepare_outlier_data(df):
+    df_outliers = prepare_dataset(df)
+    return df_outliers
+
+def prepare_non_outlier_data(df):
+    df_outliers = process_data(df)
+    return df_outliers
+
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+
+# Define app layout
+app.layout = html.Div([
+    html.H1("Customer Segmentation Dashboard"),
+    dcc.Upload(
+        id = 'upload-data',
+        children = html.Div([
+            'Drag and Drop or ',
+            html.A('Select CSV File')
+        ]),
+        style ={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple = False
+    ),
+    html.Div([
+        dcc.Graph(id = 'customer-segments-scatter'),
+        dcc.Graph(id = 'customer-segments-scatter-2')
+    ], style = {'display': 'flex'}),
+])
+
+@app.callback(
+    [Output('customer-segments-scatter', 'figure'),
+     Output('customer-segments-scatter-2', 'figure')],
+    [Input('upload-data', 'contents')]
+)
+def update_output(contents):
+    if contents is not None:
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        
+        Out = prepare_outlier_data(df)
+        n_Out = prepare_non_outlier_data(Out)
+        Out.to_csv('Out.csv', sep=',', index=False, header=True)
+        n_Out.to_csv('No_Out.csv', sep=',', index=False, header=True)
+        
+        # Plot 3D scatter plot of customer segments for both graphs
+        fig = go.Figure(data = [go.Scatter3d(
+            x = Out['Recency'],
+            y = Out['Frequency'],
+            z = Out['Amount'],
+            mode = 'markers',
+            marker = dict(
+                size = 5,
+                color = Out['Amount'],  # Color by cluster label
+                opacity = 0.8,
+                colorscale = 'Viridis'
+            )
+        )])
+
+        fig2 = go.Figure(data = [go.Scatter3d(
+            x = n_Out['Recency'],
+            y = n_Out['Frequency'],
+            z = n_Out['Amount'],
+            mode ='markers',
+            marker = dict(
+                size = 5,
+                color = n_Out['Frequency'],  # Color by cluster label
+                opacity = 0.8,
+                colorscale = 'Viridis'
+            )
+        )])
+        
+
+        fig.update_layout(scene = dict(
+                    xaxis_title = 'Recency',
+                    yaxis_title = 'Frequency',
+                    zaxis_title = 'Amount'),
+                title='3D Plot of Recency, Frequency, and Amount - With outliers',
+                scene_camera=dict(
+                        up=dict(x=0, y=0, z=1),
+                        center=dict(x=0, y=0, z=0),
+                        eye=dict(x=-1.25, y=-1.25, z=1.25) 
+                ),
+                width=1000,
+                height=700 
+                )
+        
+        fig2.update_layout(scene = dict(
+                    xaxis_title ='Recency',
+                    yaxis_title ='Frequency',
+                    zaxis_title ='Amount'),
+                title ='3D Plot of Recency, Frequency, and Amount - Without outliers',
+                scene_camera=dict(
+                        up=dict(x=0, y=0, z=1),
+                        center=dict(x=0, y=0, z=0),
+                        eye=dict(x=-1.25, y=-1.25, z=1.25) 
+                ),
+                width=1000,
+                height=700 
+                )
+        
+        return fig, fig2
+    else:
+        return {}, {}
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.run_server(debug=True)
