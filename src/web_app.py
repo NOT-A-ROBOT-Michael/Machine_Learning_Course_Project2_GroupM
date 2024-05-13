772d832cb96582736700dda9d12850fe2a41810f
KCSchmutz <577511@student.belgiumcampus.ac.za>
2024-05-13 18:16:49 +0200

    Karls
    changes

--- src/web_app.py
+++ src/web_app.py
@@ -1,7 +1,7 @@
+"begin"
 import pandas as pd
 import dash
 from dash import dcc, html, Input, Output
-from dash import callback
 import base64
 import io
 import plotly.graph_objs as go
@@ -16,7 +16,6 @@ sns.color_palette("rocket", as_cmap=True)
 "outliers, no_outliers"
 df = pd.DataFrame()
 "code to train models"
-
 def prepare_outlier_data(df):
     df_temp = prepare_dataset(df)
     return df_temp
@@ -29,22 +28,18 @@ def train_data(df):
     df_temp = create_finalmodel(df)
     return df_temp
 
-"the slider functionallity"
-"app-style"
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
 server = app.server
 
-# Define app layout
 app.layout = html.Div([
     html.H1("Customer Segmentation Dashboard"),
     dcc.Upload(
-        id = 'upload-data',
-        children = html.Div([
+        id='upload-data',
+        children=html.Div([
             'Drag and Drop or ',
             html.A('Select CSV File')
         ]),
-        style ={
+        style={
             'width': '100%',
             'height': '60px',
             'lineHeight': '60px',
@@ -54,38 +49,49 @@ app.layout = html.Div([
             'textAlign': 'center',
             'margin': '10px'
         },
-        multiple = False
+        multiple=False
     ),
+    html.P("Filter based on amount"),
     html.Div([
-        dcc.Graph(id = 'customer-segments-scatter'),
-        dcc.Graph(id = 'customer-segments-scatter-2'),
-    ], style = {'display': 'flex'}),
-    html.H1("Customer Amount:"),
+        dcc.RangeSlider(
+            id='amount-slider',
+            min=0,
+            max=10000,
+            value=[0, 10000],
+            marks={i: str(i) for i in range(0, 10001, 2000)}
+        )
+    ], style={'marginBottom': '30px'}),
+    html.P("Filter based on frequency"),
     html.Div([
-    dcc.Slider(0, 85000, 5000,
-               value=60000,
-               id='my-slider'
-    ),
-    html.Div(id='slider-output-container')
-    ]),
+        dcc.RangeSlider(
+            id='frequency-slider',
+            min=0,
+            max=200,
+            value=[0, 200],
+            marks={i: str(i) for i in range(0, 201, 50)}
+        )
+    ], style={'marginBottom': '30px'}),
+    html.Div([
+        dcc.Graph(id='customer-segments-scatter'),
+        dcc.Graph(id='customer-segments-scatter-2')
+    ], style={'display': 'flex'}),
 ])
 
 @app.callback(
-    Output('slider-output-container', 'slider-output-container'),
-    Output('customer-segments-scatter', 'figure'),
-    Output('customer-segments-scatter-2', 'figure'),
-    [Input('my-slider', 'value'),
-    Input('upload-data', 'contents')],
+    [Output('customer-segments-scatter', 'figure'),
+     Output('customer-segments-scatter-2', 'figure')],
+    [Input('upload-data', 'contents'),
+     Input('amount-slider', 'value'),
+     Input('frequency-slider', 'value')]
 )
-
-def update_output(value, contents):
+def update_output(contents, amount_range, frequency_range):
     if contents is not None:
         content_type, content_string = contents.split(',')
         decoded = base64.b64decode(content_string)
         df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
 
-        temp_Out=prepare_outlier_data(df)
-        temp_n_Out=prepare_non_outlier_data(temp_Out)
+        temp_Out = prepare_outlier_data(df)
+        temp_n_Out = prepare_non_outlier_data(temp_Out)
 
         temp_Out.to_csv('Out.csv', sep=',', index=False, header=True)
         temp_n_Out.to_csv('No_Out.csv', sep=',', index=False, header=True)
@@ -95,60 +101,58 @@ def update_output(value, contents):
         Out.to_csv('Out.csv', sep=',', index=False, header=True)
         n_Out.to_csv('No_Out.csv', sep=',', index=False, header=True)
 
-        
-        # Plot 3D scatter plot of customer segments for both graphs
-        fig = go.Figure(data = [go.Scatter(
-            x = Out['Amount'],
-            y = Out['Frequency'],
-            mode = 'markers',
-            marker = dict(
-                size = 5,
-                color = Out['Cluster'],  # Color by cluster label
-                opacity = 0.8,
-                colorscale = 'Viridis'
+        # Filter data based on amount and frequency ranges
+        Out = Out[(Out['Amount'] >= amount_range[0]) & (Out['Amount'] <= amount_range[1]) &
+                  (Out['Frequency'] >= frequency_range[0]) & (Out['Frequency'] <= frequency_range[1])]
+        n_Out = n_Out[(n_Out['Amount'] >= amount_range[0]) & (n_Out['Amount'] <= amount_range[1]) &
+                      (n_Out['Frequency'] >= frequency_range[0]) & (n_Out['Frequency'] <= frequency_range[1])]
+
+        # Plot 2D scatter plot of customer segments for both graphs
+        fig = go.Figure(data=[go.Scatter(
+            x=Out['Frequency'],
+            y=Out['Amount'],
+            mode='markers',
+            marker=dict(
+                size=5,
+                color=Out['Cluster'],  # Color by cluster label
+                opacity=0.8,
+                colorscale='Viridis'
             )
         )])
 
-        fig2 = go.Figure(data = [go.Scatter(
-            x = n_Out['Amount'],
-            y = n_Out['Frequency'],
-            mode ='markers',
-            marker = dict(
-                size = 5,
-                color = n_Out['Cluster'],  # Color by cluster label
-                opacity = 0.8,
-                colorscale = 'Viridis'
+        fig2 = go.Figure(data=[go.Scatter(
+            x=n_Out['Frequency'],
+            y=n_Out['Amount'],
+            mode='markers',
+            marker=dict(
+                size=5,
+                color=n_Out['Cluster'],  # Color by cluster label
+                opacity=0.8,
+                colorscale='Viridis'
             )
         )])
-        fig.update_layout(scene = dict(
-                    xaxis_title = 'Amount',
-                    yaxis_title = 'Frequency'),
-                title='Scatter Plot of Frequency vs Amount - With outliers',
-                scene_camera=dict(
-                        up=dict(x=0, y=0, z=1),
-                        center=dict(x=0, y=0, z=0),
-                        eye=dict(x=-1.25, y=-1.25, z=1.25) 
-                ),
-                width=1000,
-                height=700 
-                )
-        fig2.update_layout(scene = dict(
-                    xaxis_title ='Amount',
-                    yaxis_title ='Frequency'),
-                title ='Scatter Plot of Frequency vs Amount - With no-outliers',
-                scene_camera=dict(
-                        up=dict(x=0, y=0, z=1),
-                        center=dict(x=0, y=0, z=0),
-                        eye=dict(x=-1.25, y=-1.25, z=1.25) 
-                ),
-                width=1000,
-                height=700 
-                )
-        return fig, fig2, {}
-        #'You have selected the customer total amount of "{}"'.format(value)
+
+        fig.update_layout(
+            title='2D Plot of Frequency and Amount - With outliers',
+            xaxis_title='Frequency',
+            yaxis_title='Amount',
+            width=1000,
+            height=700
+        )
+
+        fig2.update_layout(
+            title='2D Plot of Frequency and Amount - Without outliers',
+            xaxis_title='Frequency',
+            yaxis_title='Amount',
+            width=1000,
+            height=700
+        )
+
+        return fig, fig2
     else:
-        return {}, {}, {}
+        return {}, {}
 
 # Run the Dash app
 if __name__ == '__main__':
     app.run_server(debug=True)
+"end"
