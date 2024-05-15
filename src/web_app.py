44908a85c2ede011e0712a935754d65637e354a1
wianvanniekerk <140880006+wianvanniekerk@users.noreply.github.com>
2024-05-15 09:20:43 +0200

    Update web_app.py

--- src/web_app.py
+++ src/web_app.py
@@ -1,4 +1,3 @@
-"begin"
 import pandas as pd
 import dash
 from dash import dcc, html, Input, Output
@@ -11,11 +10,9 @@ from preprocess_data import process_data
 from train_models import create_finalmodel
 import dash_bootstrap_components as dbc
 
-"palettes"
 sns.color_palette("rocket", as_cmap=True)
-"outliers, no_outliers"
 df = pd.DataFrame()
-"code to train models"
+
 def prepare_outlier_data(df):
     df_temp = prepare_dataset(df)
     return df_temp
@@ -51,14 +48,16 @@ app.layout = html.Div([
         },
         multiple=False
     ),
+    html.H1("Cluster Visualisation", style={'textAlign': 'center'}),
+    html.P(style={'marginBottom': '30px'}),
     html.P("Filter based on amount (USD)"),
     html.Div([
         dcc.RangeSlider(
             id='amount-slider',
             min=0,
-            max=10000,
-            value=[0, 10000],
-            marks={i: str(i) for i in range(0, 10001, 2000)}
+            max=80000,
+            value=[0, 80000],
+            marks={i: str(i) for i in range(0, 80001, 20000)}
         )
     ], style={'marginBottom': '30px'}),
     html.P("Filter based on frequency (days)"),
@@ -71,20 +70,28 @@ app.layout = html.Div([
             marks={i: str(i) for i in range(0, 201, 50)}
         )
     ], style={'marginBottom': '30px'}),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='customer-segments-scatter'), width=6),
+        dbc.Col(dcc.Graph(id='customer-segments-scatter-2'), width=6)
+    ]),
     html.Div([
-        dcc.Graph(id='customer-segments-scatter'),
-        dcc.Graph(id='customer-segments-scatter-2')
-    ], style={'display': 'flex'}),
+        html.Button("Download Filtered Data", id="btn-download", n_clicks=0,
+                    style={'display': 'block', 'margin': '0 auto', 'marginBottom': '30px'}),
+        dcc.Download(id="download-dataframe-csv")
+    ], style={'textAlign': 'center'}),
 ])
 
 @app.callback(
     [Output('customer-segments-scatter', 'figure'),
-     Output('customer-segments-scatter-2', 'figure')],
+     Output('customer-segments-scatter-2', 'figure'),
+     Output('download-dataframe-csv', 'data')],
     [Input('upload-data', 'contents'),
      Input('amount-slider', 'value'),
-     Input('frequency-slider', 'value')]
+     Input('frequency-slider', 'value'),
+     Input('btn-download', 'n_clicks')]
 )
-def update_output(contents, amount_range, frequency_range):
+def update_output(contents, amount_range, frequency_range, n_clicks):
+    download_data = None
     if contents is not None:
         content_type, content_string = contents.split(',')
         decoded = base64.b64decode(content_string)
@@ -93,42 +100,37 @@ def update_output(contents, amount_range, frequency_range):
         temp_Out = prepare_outlier_data(df)
         temp_n_Out = prepare_non_outlier_data(temp_Out)
 
-        temp_Out.to_csv('Out.csv', sep=',', index=False, header=True)
-        temp_n_Out.to_csv('No_Out.csv', sep=',', index=False, header=True)
-
         Out = train_data(temp_Out)
         n_Out = train_data(temp_n_Out)
-        Out.to_csv('Out.csv', sep=',', index=False, header=True)
-        n_Out.to_csv('No_Out.csv', sep=',', index=False, header=True)
 
         # Filter data based on amount and frequency ranges
-        Out = Out[(Out['Amount'] >= amount_range[0]) & (Out['Amount'] <= amount_range[1]) &
-                  (Out['Frequency'] >= frequency_range[0]) & (Out['Frequency'] <= frequency_range[1])]
-        n_Out = n_Out[(n_Out['Amount'] >= amount_range[0]) & (n_Out['Amount'] <= amount_range[1]) &
-                      (n_Out['Frequency'] >= frequency_range[0]) & (n_Out['Frequency'] <= frequency_range[1])]
+        Out_filtered = Out[(Out['Amount'] >= amount_range[0]) & (Out['Amount'] <= amount_range[1]) &
+                           (Out['Frequency'] >= frequency_range[0]) & (Out['Frequency'] <= frequency_range[1])]
+        n_Out_filtered = n_Out[(n_Out['Amount'] >= amount_range[0]) & (n_Out['Amount'] <= amount_range[1]) &
+                               (n_Out['Frequency'] >= frequency_range[0]) & (n_Out['Frequency'] <= frequency_range[1])]
 
         # Plot 2D scatter plot of customer segments for both graphs
         fig = go.Figure(data=[go.Scatter(
-            x=Out['Frequency'],
-            y=Out['Amount'],
+            x=Out_filtered['Frequency'],
+            y=Out_filtered['Amount'],
             mode='markers',
             marker=dict(
                 size=5,
-                color=Out['Cluster'],  # Color by cluster label
+                color=Out_filtered['Cluster'],  # Color by cluster label
                 opacity=0.8,
-                colorscale='Viridis'
+                colorscale='Jet'
             )
         )])
 
         fig2 = go.Figure(data=[go.Scatter(
-            x=n_Out['Frequency'],
-            y=n_Out['Amount'],
+            x=n_Out_filtered['Frequency'],
+            y=n_Out_filtered['Amount'],
             mode='markers',
             marker=dict(
                 size=5,
-                color=n_Out['Cluster'],  # Color by cluster label
+                color=n_Out_filtered['Cluster'],  # Color by cluster label
                 opacity=0.8,
-                colorscale='Viridis'
+                colorscale='Jet'
             )
         )])
 
@@ -136,7 +138,7 @@ def update_output(contents, amount_range, frequency_range):
             title='2D Plot of Frequency and Amount - With outliers',
             xaxis_title='Frequency',
             yaxis_title='Amount',
-            width=1000,
+            width=700,
             height=700
         )
 
@@ -144,15 +146,19 @@ def update_output(contents, amount_range, frequency_range):
             title='2D Plot of Frequency and Amount - Without outliers',
             xaxis_title='Frequency',
             yaxis_title='Amount',
-            width=1000,
+            width=700,
             height=700
         )
 
-        return fig, fig2
+        # Prepare data for download
+        if n_clicks > 0:
+            filtered_data = pd.concat([Out_filtered, n_Out_filtered])
+            download_data = dcc.send_data_frame(filtered_data.to_csv, "filtered_data.csv")
+
+        return fig, fig2, download_data
     else:
-        return {}, {}
+        return {}, {}, None
 
 # Run the Dash app
 if __name__ == '__main__':
     app.run_server(debug=True)
-"end"
